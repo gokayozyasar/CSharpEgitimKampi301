3195ab20ccda0410043674866780e84cea2dfbfe
Gokay <gokayozyasar@gmail.com>
2024-11-30 21:21:41 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int categoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+
+    }
+}
+/*
+Field-Variable-Property
+*/
+
+/* 
+
+
+*/
+
